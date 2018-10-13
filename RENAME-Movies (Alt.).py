dc35791c3f488fd3f618ad498554a133c15726dc
krishna-alagiri <krishna.alagiri03@gmail.com>
2018-10-13 23:03:26 +0530

    More Bugs ;_;

--- RENAME-Movies (Alt.).py
+++ RENAME-Movies (Alt.).py
@@ -13,17 +13,31 @@ def FindYear(Str):
         yr = "(----)"
     return(yr)
 
-ia = IMDb()
-s_result = ia.search_movie('The Untouchables')
-movies = []
-str1="Movie\n=====\nTitle: "
-for movie in s_result:
-    str2 = (movie.summary())
-    str2=str2.replace(str1,"")
-    str2=str2.replace("\n","")
-    str2=str2.replace("\"","")
-    str2=str2.replace("\"","")
-    movies.append(str2)
-    year_str=FindYear(str2)
-    print(year_str)
-print(movies)
+def lastIndexOf(str1,toFind):
+    index = len(str1)-1
+    i = 0
+    for ch in str1:
+        if(ch == toFind):
+            index = i
+        i+=1
+    return(index)
+
+def main_imdb(str2):
+    ia = IMDb()
+    s_result = ia.search_movie('The Untouchables')
+    movies = []
+    str1="Movie\n=====\nTitle: "
+    for movie in s_result:
+        str2 = (movie.summary())
+        str2=str2.replace(str1,"")
+        str2=str2.replace("\n","")
+        str2=str2.replace("\"","")
+        str2=str2.replace("\"","")
+        year_str=FindYear(str2)
+        str2 = str2[:lastIndexOf(str2,",")]
+        str2 = str2.split("(",1)[0]
+        str2 = str2.strip()
+        movies.append(str2+" "+year_str)
+    return(movies)
+
+main_imdb(str2)
