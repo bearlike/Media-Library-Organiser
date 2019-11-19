a7a571cb8653d58cd617ab33ab591d586180b34d
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2019-11-20 00:19:32 +0530

    Added Functionality

--- RENAME-Movies.py
+++ RENAME-Movies.py
@@ -1,15 +1,16 @@
-##=============================================================================
-##  Code Written by ~KK~
-##  To Automate the boring process of renaming files for your TV Shows library
-##  Useful for organising bulk media files or frequent updation of
-##  your XBMC library (Like Kodi, Plex and OSMC)
-##=============================================================================
+"""
+  Code Written by ~KK~
+  To Automate the boring process of renaming files for your TV Shows library
+  Useful for organising bulk media files or frequent updation of
+  your XBMC library (Like Kodi, Plex and OSMC)
+"""
 import os
 import re
-import msvcrt
+import msvcrt, shutil
 from imdb import IMDb
 from similarity.damerau import Damerau
 
+# Returns most closest Movie name
 def find_most_apt(name, movies):
     damerau = Damerau()
     deg = []
@@ -24,6 +25,7 @@ def find_most_apt(name, movies):
         mostapt = name
     return(mostapt)
 
+# Find any URLs present in FileName
 def Find(string):
     url = re.findall('www.(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\)]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
     return (url[0])
@@ -37,6 +39,7 @@ def lastIndexOf(str1,toFind):
         i+=1
     return(index)
 
+# Returns a List Movie name and release year using imDB from Old_FileName
 def main_imdb(str21):
     ia = IMDb()
     s_result = ia.search_movie(str21)
@@ -51,6 +54,7 @@ def main_imdb(str21):
             movies.append(str2+" ("+str(year_str)+")")
     return(movies)
 
+# Remove Illegal Name characters
 def removeIllegal(str):
     str=str.replace('<',"")
     str=str.replace('>',"")
@@ -63,6 +67,7 @@ def removeIllegal(str):
     str=str.replace('*',"")
     return(str)
 
+# Print Program Title
 def Title():
     os.system("cls")
     os.system('mode con: cols=75 lines=30')
@@ -83,7 +88,7 @@ def Title():
     print("           |___/                         ")
     print("")
 
-
+# Primitive FileName Formatting
 def FormatStr(temp):
     rest = temp
     if ".1080p" in temp:
@@ -108,87 +113,87 @@ def FormatStr(temp):
     rest = rest.replace(")","")
     return(rest.strip())
 
-#Driver Code
-Title()
-try:
-    os.mkdir("Input")
-except FileExistsError:
-    pass
-try:
-    os.mkdir("Input\\Movies")
-except FileExistsError:
-    pass
-try:
-    os.mkdir("Output")
-except FileExistsError:
-    pass
-path = "Input\\Movies"
-i=0
-ErrorFlag=0
-FileFlag=0
-files = os.listdir(path)
-for file in files:
-    temp = file
-    extn = file[(len(file)-4) : len(file)]
+# Driver Code
+def main():
+    Title()
+    try:
+        os.mkdir("Input")
+    except FileExistsError:
+        pass
+    try:
+        os.mkdir("Input\\Movies")
+    except FileExistsError:
+        pass
+    try:
+        os.mkdir("Output")
+    except FileExistsError:
+        pass
+    path = "Input\\Movies"
+    i=0
+    ErrorFlag=0
+    FileFlag=0
+    files = os.listdir(path)
+    for file in files:
+        temp = file
+        extn = file[(len(file)-4) : len(file)]
+        if(file.endswith(".mp4") or file.endswith(".mkv") or file.endswith(".srt")) :
+            Title()
+            print(str(i)+" File(s) Processed....")
+            try:
+                url = Find(temp)
+                # print(url)
+                temp = temp.replace(url,"")
+            except:
+                pass
+            rest = FormatStr(temp.strip())
+            year_str =  '('+ rest[len(rest)-4 : len(rest)] +')'
+            rest = rest[0:len(rest)-4]
+            Final = rest + year_str
+            print("Derived: ",Final)
+            movies = main_imdb(rest + year_str)
+            rest = find_most_apt(Final, movies) # Sometimes causes error
+            rest = removeIllegal(Final)
+            Final = rest + extn
+            print("Most Apt: ",Final)
+            print()
+            # Rename from old -> new happens below
+            path_new = os.getcwd() + "\\Output\\Movies\\" + rest
+            try:
+                os.mkdir("Output")
+            except FileExistsError:
+                pass
+            try:
+                os.mkdir("Output\\Movies")
+            except FileExistsError:
+                pass
+            try:
+                os.mkdir(path_new)
+            except FileExistsError:
+                pass
+            try:
+                os.rename(os.path.join(path, file), os.path.join(path_new, Final ))
+            except:
+                print("Error - File Already Exist: "+rest )
+                FileFlag=1
+                ErrorFlag=1
+                i=i-1;
+            # print(file)
+            i=i+1
+    # Result Generation
+    Title()
+    print ("All Files Processed...")
+    if(FileFlag==1):
+        print("Solution: Try again after removing the above file(s) from Output folder")
+    if(i>0 or ErrorFlag==1):
+        print(str(i)+" File(s) Renamed and Organised Successfully")
+        # if(i>0): os.system("explorer.exe " + str(os.getcwd() + "\Output\\"))
+    else:
+        print("No Media File Found in Input Folder")
 
-    if(file.endswith(".mp4") or file.endswith(".mkv") or file.endswith(".srt")) :
-        Title()
-        print(str(i)+" File(s) Processed....")
-        url = Find(temp)
-        # print(url)
-        temp = temp.replace(url,"")
-        rest = FormatStr(temp.strip())
-        year_str =  '('+ rest[len(rest)-4 : len(rest)] +')'
-        rest = rest[0:len(rest)-4]
-        Final = rest + year_str
-        print("Derived: ",Final)
-        movies = main_imdb(rest + year_str)
-        rest = find_most_apt(Final, movies) # Sometimes causes error
-        rest = removeIllegal(Final)
-        Final = rest + extn
-        print("Most Apt: ",Final)
-        print()
 
-###############################################################################
-##RENAME HAPPENS HERE
-        path_new = os.getcwd() + "\\Output\\Movies\\" + rest
-        try:
-            os.mkdir("Output")
-        except FileExistsError:
-            pass
-        try:
-            os.mkdir("Output\\Movies")
-        except FileExistsError:
-            pass
-        try:
-            os.mkdir(path_new)
-        except FileExistsError:
-            pass
-
-        try:
-            os.rename(os.path.join(path, file), os.path.join(path_new, Final ))
-        except:
-            print("Error - File Already Exist: "+rest )
-            FileFlag=1
-            ErrorFlag=1
-            i=i-1;
-        
-        # print(file)
-        i=i+1
-
-###############################################################################
-
-##RESULT GENERATION
-
-Title()
-print ("All Files Processed...")
-if(FileFlag==1):
-    print("Solution: Try again after removing the above file(s) from Output folder")
-if(i>0 or ErrorFlag==1):
-    print(str(i)+" File(s) Renamed and Organised Successfully")
-    if(i>0): os.system("explorer.exe " + str(os.getcwd() + "\Output\\"))
-else:
-    print("No Media File Found in Input Folder")
-print("Enter any key to exit...")
-print()
-msvcrt.getch()
+if __name__ == '__main__':
+    main()
+    print("Moving files..")
+    print("Enter any key to exit...")
+    print()
+    msvcrt.getch()
--- RENAME-Series.py
+++ RENAME-Series.py
@@ -1,15 +1,19 @@
-##=============================================================================
-##  Code Written by ~KK~
-##  To Automate the boring process of renaming files for your TV Shows library
-##  Useful for organising bulk media files or frequent updation of
-##  your XBMC library (Like Kodi, Plex and OSMC)
-##=============================================================================
+"""
+  Code Written by ~KK~
+  To Automate the boring process of renaming files for your TV Shows library
+  Useful for organising bulk media files or frequent updation of
+  your XBMC library (Like Kodi, Plex and OSMC)
+"""
 import os
 import re
 import msvcrt
+import shutil
+import getopt
+import sys
 from imdb import IMDb
 from similarity.damerau import Damerau
 
+# Title
 def Title():
     os.system('mode con: cols=90 lines=30')
     os.system("cls")
@@ -30,6 +34,8 @@ def Title():
     print("           |___/                         ")
     print("")
 
+
+# Find most apt name in Series List
 def find_most_apt(name, series):
     damerau = Damerau()
     deg = []
@@ -42,6 +48,8 @@ def find_most_apt(name, series):
     mostapt = series[indd]
     return(mostapt)
 
+
+# Retrieves name from imDB
 def main_imdb(str21):
     ia = IMDb()
     s_result = ia.search_movie(str21)
@@ -52,6 +60,8 @@ def main_imdb(str21):
             series.append(str2)
     return(series)
 
+
+# Remove illegal characters from file name
 def removeIllegal(str):
     str=str.replace('<',"")
     str=str.replace('>',"")
@@ -65,14 +75,18 @@ def removeIllegal(str):
     str=str.strip()
     return(str)
 
+
 def hasX(inputString):
     return bool(re.search(r'\dx\d', inputString) or re.search(r'\d x \d', inputString))
+
 
 def hasSE(inputString):
     return bool(re.search(r'S\d\dE\d\d', inputString) or re.search(r'S\dE\d\d', inputString) or re.search(r's\d\de\d\d', inputString) or re.search(r's\de\d\d', inputString))
 
+
 def hasSEP(inputString):
     return bool(re.search(r'S\d\dEP\d\d', inputString) or re.search(r'S\dEP\d\d', inputString) or re.search(r's\d\dep\d\d', inputString) or re.search(r's\dep\d\d', inputString))
+
 
 def FindName(inputString):
     inputString = inputString.replace(' x ', 'x', 1)
@@ -83,6 +97,7 @@ def FindName(inputString):
         Name = Name.replace('.',' ')
         Name = Name.strip()
         return str(Name)
+
 
 def FindDet(inputString):
     inputString = inputString.replace(' x ', 'x', 1)
@@ -97,163 +112,223 @@ def FindDet(inputString):
             return str(Det)
         i=i+1
 
+
 def FindSeason(inputString):
     Det = FindDet(inputString)
     Season = Det.split('x')[0]
     return str(Season)
+
 
 def FindEpisode(inputString):
     Det = FindDet(inputString)
     Episode = Det.split('x')[1]
     return str(Episode)
 
+
 def AddZero(inputString):
     if(int(inputString) <10):
         return str('0' + str(int(inputString)))
     return(inputString)
 
+
 def split_line(text):
     words = text.split()
     return words
-###############################################################################
-##MAIN
-Title()
-try:
-    os.mkdir(str(os.getcwd())+"\Input")
-except:
-    pass
-try:
-    os.mkdir(str(os.getcwd())+"\\Input\\Series")
-except:
-    pass
-path = str(os.getcwd())+"\\Input\\Series\\"
-path_new = path_new_1 = rest = Final = "NULL"
-NAME=""
-Copy=""
-i=0
-ErrorFlag=0
-FileFlag=0
-files = os.listdir(path)
-print("Reading Files....")
-for file in files:
-    CheckFlag=0
-    temp = file
-    extn = file[(len(file)-4) : len(file)]
-###############################################################################
-    if (file.endswith(".mp4") or file.endswith(".mkv") or file.endswith(".srt")):
-        Title()
-        print(str(i)+" File(s) Processed....")
-        rest = temp.split(extn,1)[0]
-        rest = rest.replace(".1080p","")
-        rest = rest.replace(".720p","")
-        rest = rest.replace("HDTV","")
-        rest = rest.replace("x264","")
-        rest = rest.replace("AAC","")
-        rest = rest.replace("E-Subs","")
-        rest = rest.replace("ESubs","")
-        rest = rest.replace("WEBRip","")
-        rest = rest.replace("WEB","")
-        rest = rest.replace("."," ")
-    ##Specifically written for'x' type Files
-        if hasX(file):
-            CheckFlag=1
-            NAME = FindName(rest)
-            SEASON = FindSeason(rest)
-            EPISODE = AddZero(FindEpisode(rest))
-            Final = "S"+AddZero(FindSeason(rest))+"E"+EPISODE+extn
 
-    ##Specifically written for 'S__E__' type Files
-        elif hasSE(file):
-            NAME=""
-            words = split_line(rest)
-            for word in words:
-                if(hasSE(word)):
-                    Final = word
-                    break
-                if(hasSEP(word)):
-                    Final = word
-                    break
+
+# Before Driver
+def init(path="NULL"):
+    Title()
+    try:
+        os.mkdir(str(os.getcwd())+"\\Input")
+    except:
+        pass
+    try:
+        os.mkdir(str(os.getcwd())+"\\Input\\Series")
+    except:
+        pass
+    main(path)
+
+
+# Driver Code
+def main(path="NULL"):
+    if path == "NULL":
+        cwd = str(os.getcwd())
+        path = cwd+"\\Input\\Series\\"
+        case = 1
+    else:
+        cwd = path
+        print(path)
+        case = 2
+    path_new = path_new_1 = rest = Final = "NULL"
+    NAME=""
+    Copy=""
+    i=0
+    ErrorFlag=0
+    FileFlag=0
+    files = os.listdir(path)
+    print("Reading Files....")
+    for file in files:
+        CheckFlag=0
+        temp = file
+        extn = file[(len(file)-4) : len(file)]
+        """Logic Starts here"""
+        # All possible media extensions go here
+        media_extensions = [
+                ".mp4",
+                ".mkv",
+                ".srt",
+                ".avi",
+                ".wmv"
+        ]
+        if (file.endswith(ex1) for ex1 in media_extensions):
+            Title()
+            print(file)
+            print(str(i)+" File(s) Processed....")
+            rest = temp.split(extn,1)[0]
+            unwanted_stuff = [
+                ".1080p",
+                ".720p",
+                "HDTV",
+                "x264",
+                "AAC",
+                "E-Subs",
+                "ESubs",
+                "WEBRip",
+                "WEB",
+                "BluRay",
+            ]
+            for stuff in unwanted_stuff:
+                rest = rest.replace(stuff,"")
+            rest = rest.replace(".", " ")
+            # Specifically written for'x' type Files
+            if hasX(file):
+                CheckFlag=1
+                NAME = FindName(rest)
+                SEASON = FindSeason(rest)
+                EPISODE = AddZero(FindEpisode(rest))
+                Final = "S"+AddZero(FindSeason(rest))+"E"+EPISODE+extn
+
+            # Specifically written for 'S__E__' type Files
+            elif hasSE(file):
+                NAME=""
+                words = split_line(rest)
+                for word in words:
+                    if(hasSE(word)):
+                        Final = word
+                        break
+                    if(hasSEP(word)):
+                        Final = word
+                        break
+                    else:
+                        NAME = NAME + word + " "
+                brackets_ = re.findall('[(]+(.*?)[)]', NAME)
+                # print(brackets_)
+                for yy in brackets_:
+                    try:
+                        NAME = NAME.replace(yy,"")
+                    except:
+                        pass
+                if(NAME != Copy):
+                    Copy = NAME
+                    series = main_imdb(NAME)
+                    NAME = find_most_apt(NAME, series)
+                    NAME = removeIllegal(NAME)
+                    NAME = NAME.strip()
+                    Restore = NAME
                 else:
-                    NAME = NAME + word + " "
-            brackets_ = re.findall('[(]+(.*?)[)]', NAME)
-            for yy in brackets_:
+                    NAME = Restore
+                TEMP=Final=Final.strip()
+                TEMP=TEMP.replace('S',"")
+                SEASON = TEMP.split('E',1)[0]
+                EPISODE = TEMP.split('E',1)[1]
+                Final = Final + extn
+                CheckFlag=1
+
+        if CheckFlag == 1:
+            if case == 1:
+                path_new = cwd + "\\Output\\Series\\" + NAME
+                path_new_1 = cwd + "\\Output\\Series\\" + NAME +"\\Season "+ str(int(SEASON))
+            elif case == 2:
+                path_new = cwd + "\\" + NAME
+                path_new_1 = cwd + "\\" + NAME +"\\Season "+ str(int(SEASON))
+
+
+        """Naming Logic Ends here"""
+        # For Testing
+        def TEST():
+            print ("\n\n")
+            print("NAME: " + NAME)
+            print("SEASON: " + SEASON)
+            print("EPISODE: " + EPISODE)
+            print("EXTN: " + extn)
+            # print("TEMP: " + temp)
+            print("REST: " + rest)
+            print("Final: " + Final)
+            print("PATH: "+ path_new)
+            print ("\n\n")
+        # TEST()    # Test Call
+        if CheckFlag == 1:
+            if case == 1:
                 try:
-                    NAME=NAME.replace(yy,"")
-                except:
+                    os.mkdir(cwd+"\\Output")
+                except FileExistsError:
                     pass
-            if(NAME != Copy):
-                Copy = NAME
-                series = main_imdb(NAME)
-                NAME = find_most_apt(NAME, series)
-                NAME = removeIllegal(NAME)
-                NAME=NAME.strip()
-                Restore = NAME
-            else:
-                NAME = Restore
-            TEMP=Final=Final.strip()
-            TEMP=TEMP.replace('S',"")
-            SEASON = TEMP.split('E',1)[0]
-            EPISODE = TEMP.split('E',1)[1]
-            Final = Final + extn
-            CheckFlag=1
+                try:
+                    os.mkdir(cwd+"\\Output\\Series")
+                except FileExistsError:
+                    pass
+            try:
+                os.mkdir(path_new)
+            except FileExistsError:
+                pass
+            try:
+                os.mkdir(path_new_1)
+            except FileExistsError:
+                pass
+            try:
+                os.rename(os.path.join(path, file), os.path.join(path_new_1, Final ))
+            except FileExistsError:
+                print("Error - File Already Exist: \\"+ NAME +"\\Season "+ str(int(SEASON)) + "\\" + Final)
+                FileFlag=1
+                ErrorFlag=1
+                i=i-1;
+            i=i+1
 
-    if CheckFlag==1:
-        path_new = os.getcwd() + "\\Output\\Series\\" + NAME
-        path_new_1 = os.getcwd() + "\\Output\\Series\\" + NAME +"\\Season "+ str(int(SEASON))
+    """Result Generation"""
+    Title()
+    print ("All Files Processed...")
+    if(FileFlag==1):
+        print("Solution: Try again after removing the above file(s) from Output folder")
+    if(i>0 or ErrorFlag==1):
+        print(str(i)+" File(s) Renamed and Organised Successfully")
+        # os.system("explorer.exe " + str(os.getcwd() + "\Output\\"))
+    else:
+        print("No Media File Found in Input Folder")
 
-###############################################################################
-    def TEST():
-        ##   TESTING
-        ## =============
-        print ("\n\n")
-        print("NAME: " + NAME)
-        print("SEASON: " + SEASON)
-        print("EPISODE: " + EPISODE)
-        print("EXTN: " + extn)
-        ##print("TEMP: " + temp)
-        print("REST: " + rest)
-        print("Final: " + Final)
-        print("PATH: "+ path_new)
-        print ("\n\n")
-    #TEST()
-    if CheckFlag==1:
-        try:
-            os.mkdir(str(os.getcwd())+"\\Output")
-        except FileExistsError:
-            pass
-        try:
-            os.mkdir(str(os.getcwd())+"\\Output\\Series")
-        except FileExistsError:
-            pass
-        try:
-            os.mkdir(path_new)
-        except FileExistsError:
-            pass
-        try:
-            os.mkdir(path_new_1)
-        except FileExistsError:
-            pass
-        try:
-            os.rename(os.path.join(path, file), os.path.join(path_new_1, Final ))
-        except FileExistsError:
-            print("Error - File Already Exist: \\"+ NAME +"\\Season "+ str(int(SEASON)) + "\\" + Final)
-            FileFlag=1
-            ErrorFlag=1
-            i=i-1;
 
-        i=i+1
+# Prints usage/help
+def usageHelp():
+    print('\nUsage:\n  RENAME-Series.py [options] <commands (or) path>')
+    print("\nGeneral Options:")
+    print("  -h \tShow help.")
+    print("  -p \tTo orgainise TV Shows in the specified directory\n")
+    sys.exit(2)
 
-###############################################################################
-##RESULT GENERATION
-Title()
-print ("All Files Processed...")
-if(FileFlag==1):
-    print("Solution: Try again after removing the above file(s) from Output folder")
-if(i>0 or ErrorFlag==1):
-    print(str(i)+" File(s) Renamed and Organised Successfully")
-    os.system("explorer.exe " + str(os.getcwd() + "\Output\\"))
-else:
-    print("No Media File Found in Input Folder")
-print("Enter any key to exit...")
-print(" ")
-msvcrt.getch()
+
+if __name__ == '__main__':
+    path = "NULL"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'p:h', ["path="])
+    except getopt.GetoptError:
+        usageHelp()
+    for opt, arg in opts:
+        if opt in ("-p", "--path"):
+            path = arg
+        elif opt == '-h':
+            usageHelp()
+    init(path)
+    print("Moving files..")
+    print("Enter any key to exit...")
+    print()
+    msvcrt.getch()
